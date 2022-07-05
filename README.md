1ef59fac7a5eb5f2dd8325b3d8253bb124ab9829
Brian Foster <brianisbrilliant@users.noreply.github.com>
2022-07-05 17:18:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Oberon
+
+Developed with Unreal Engine 5
---
+++ Source/Oberon/OberonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OberonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OBERON_API AOberonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
